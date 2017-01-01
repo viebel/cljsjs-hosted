2b54c1181b8f075845cbef48c02ff0250dd57c19
Yehonathan Sharvit <viebel@gmail.com>
2017-01-01 08:30:09 +0200

    react, react-dom, react-dom-server, react-with-add-ons version 15.4.0-0

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject cljsjs-hosted "0.0.1"
+  :description "cljsjs hosted files"
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [org.clojure/clojurescript "1.9.293"]
+                 [cljsjs/react "15.4.0-0"]
+                 [cljsjs/react-dom "15.4.0-0"]
+                 [cljsjs/react-dom-server "15.4.0-0"]
+                 [cljsjs/react-with-addons "15.4.0-0"]]
+  :source-paths ["src"]
+    )
