aa35acf34d6a2a23ac958892e121ec9a6cf80d2c
Yehonathan Sharvit <viebel@gmail.com>
2017-01-02 08:42:16 +0200

    highchart 5.0.4-0

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
                  [org.clojure/clojurescript "1.9.293"]
                  [cljsjs/moment "2.15.2-3"]
+                 [cljsjs/highcharts "5.0.4-0"]
                  [cljsjs/react "15.4.0-0"]
                  [cljsjs/react-dom "15.4.0-0"]
                  [cljsjs/react-dom-server "15.4.0-0"]
